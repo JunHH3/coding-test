9144f53f3e7e8a8195d1dd3dafa2094afd16a999
JunHH3 <junhh02@gmail.com>
2026-06-25 01:56:38 +0900

    둘 중 큰 수 출력 문제

---
+++ src/main/java/jun/coding_test/javaclass/CT11.java
@@ -0,0 +1,18 @@
+package jun.coding_test.javaclass;
+
+import java.util.Scanner;
+
+public class CT11 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        if (a > b ) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}

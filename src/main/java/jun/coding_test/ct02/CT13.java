fcd9320ac07e0ef9921603aa4347151a4deedab2
JH <tarakaing@naver.com>
2026-06-29 21:58:10 +0900

    배열 전체 합 구하기

--- src/main/java/jun/coding_test/ct02/CT13.java
+++ src/main/java/jun/coding_test/ct02/CT13.java
@@ -1,4 +1,34 @@
 package jun.coding_test.ct02;
 
+import java.util.Scanner;
+
 public class CT13 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        int m = s.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            int start = s.nextInt();
+            int end = s.nextInt();
+
+            int count = 0;
+
+            for (int j = start - 1; j <= end - 1; j++) {
+                if (arr[j] % 2 == 0) {
+                    count++;
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
 }

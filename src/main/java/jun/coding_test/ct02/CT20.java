9cdd945048cb36f26e16d0f2fa8cd1d23a6b94a8
JunHH3 <tarakaing@naver.com>
2026-07-15 13:24:56 +0900

    CT20 짝수 개수 세기

---
+++ src/main/java/jun/coding_test/ct02/CT20.java
@@ -0,0 +1,24 @@
+package jun.coding_test.ct02;
+
+import java.util.Scanner;
+
+public class CT20 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+
+            if (arr[i] % 2 == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+
+
+}

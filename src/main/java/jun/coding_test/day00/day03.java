fdf918ad09b318fca8f3c631bcb5bc1b5e9bc248
JunHH3 <tarakaing@naver.com>
2026-09-01 20:44:33 +0900

    day03 오완배열누적합

---
+++ src/main/java/jun/coding_test/day00/day03.java
@@ -0,0 +1,15 @@
+package jun.coding_test.day00;
+
+public class day03 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        int[] arr = {3, 7, 2, 9, 5};
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}

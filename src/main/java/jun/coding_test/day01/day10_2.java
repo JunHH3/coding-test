9e949567cafd1153be6c8f27ce41d276871a4ffd
JunHH3 <tarakaing@naver.com>
2026-09-15 10:48:43 +0900

    day10 Set Map 기초

---
+++ src/main/java/jun/coding_test/day01/day10_2.java
@@ -0,0 +1,19 @@
+package jun.coding_test.day01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class day10_2 {
+    public static void main(String[] args) {
+
+        Map<String, Integer> scores = new HashMap<>();
+
+        scores.put("java", 90);
+        scores.put("spring", 80);
+        scores.put("sql", 85);
+
+        if (scores.containsKey("java")) {
+            System.out.println(scores.get("java"));
+        }
+    }
+}
